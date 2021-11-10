a422bf4e6c1bc3eb6a4a77b47c0ff34588ce81d5
Arthur <5019arjin@gmail.com>
2021-11-10 09:23:36 -0600

    comment removal

--- zipfslaw/Sorting.h
+++ zipfslaw/Sorting.h
@@ -11,7 +11,6 @@ class Sorting
 		bool fileFind();
 		//formatting to remove symbols like -, . , , , ;, ! all that stuff
 		std::string reformat(std::string filename);
-		//reformat should return a file
 		std::string counting();
 };
 
