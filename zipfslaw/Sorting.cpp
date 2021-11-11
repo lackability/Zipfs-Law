5242accd6142bd7357805da85fff092489a62e5b
Arthur <5019arjin@gmail.com>
2021-11-11 14:36:57 -0600

    reading in works, encoding error with symbol "ΓÇö" I.E. the long dash.

--- zipfslaw/Sorting.cpp
+++ zipfslaw/Sorting.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
+#include <string>
 #include "Sorting.h"
 
 std::string Sorting::fileRequest()
@@ -38,9 +39,16 @@ std::string Sorting::reformat(std::string filename)
 {
 	//formatting to remove symbols like -, . , , , ;, ! all that stuff, dont care about grammar, ijust pure words
 
-	std::ifstream in_file("textsrc/" + filename);
-	return std::string((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+	std::fstream in_file;
+	in_file.open("textsrc/" + filename);
+	std::string converted = std::string((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+	for (int i = 0; i < converted.length(); i++) {
+		if (converted[i] == '—') {
+			converted.erase(i);
+		}
 
+	}
+	return converted;
 };
 
 
